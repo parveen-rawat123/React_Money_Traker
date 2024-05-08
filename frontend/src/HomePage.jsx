692df1510238bb554f19ffd99d085883e186e2db
praveen singh <rawatpraveen477@gmail.com>
2024-05-08 07:54:04 +0530

    add second section

--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -1,19 +1,27 @@
-import { Link } from "react-router-dom"
-import Navbaar from "./Navbaar"
-import Section_two from "./Section_two"
+import { Link } from "react-router-dom";
+import Navbaar from "./Navbaar";
+import Section_two from "./Section_two";
 const HomePage = () => {
   return (
     <>
-    <Navbaar/>
-    <div className="h-[89vh] Homepage flex  justify-center items-center pb-16 md:w-full overflow-hidden relative">
+      <Navbaar />
+      <div className="h-[89vh] Homepage flex  justify-center items-center pb-16 md:w-full overflow-hidden relative">
         <div className="p-4 relative z-10 w-4/5 text-center md:max-w-full">
-             <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Your Financial Dashboard
-                </h1>
-            <p className="mt-4 font-sans  md:text-xl  px-28 text-neutral-300 font-semibold">Effortlessly manage your finances with our intuitive dashboard. Gain insights, track expenses, and take control of your financial future. Whether budgeting for the month ahead or planning for the long term.</p>
-        <button className="mt-8 px-8 py-2 bg-white border border- rounded-full text-[1.2rem] text-gray-900 cursor-pointer hover:text-black font-semibold"> <Link to="/DashBord">Get Started &gt;&gt;&gt;</Link></button> </div>
-    </div>
-    <Section_two/>
+          <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+            Your Financial Dashboard
+          </h1>
+          <p className="mt-4 font-sans  md:text-xl  px-28 text-neutral-300 font-semibold">
+            Effortlessly manage your finances with our intuitive dashboard. Gain
+            insights, track expenses, and take control of your financial future.
+            Whether budgeting for the month ahead or planning for the long term.
+          </p>
+          <button className="mt-8 px-8 py-[0.55rem] bg-white  border- rounded-full text-[1.2rem] text-gray-900 cursor-pointer hover:text-white font-semibold btn">
+            <Link to="/DashBord">Get Started &gt;&gt;&gt;</Link>
+          </button>
+        </div>
+      </div>
+      <Section_two />
     </>
-  )
-}
-export default HomePage
+  );
+};
+export default HomePage;
--- frontend/src/Section_two.jsx
+++ frontend/src/Section_two.jsx
@@ -1,17 +1,24 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 const Section_two = () => {
+  useEffect(()=>{
+    AOS.init({duration:'1500', delay : 100,
+    })
+  },[])
   return (
    <>
    <div className="h-[25rem] w-full flex justify-center items-center">
      <div className="flex  items-center justify-center w-[60rem]">
-        <img src="image_one.jpg" alt="image"  width={700} height={700}/>
-        <p className="text-[1.5rem]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, praesentium aspernatur illum necessitatibus sed recusandae nostrum accusantium nemo ullam odit?</p>
+        <img src="image_one.jpg" alt="image"  width={500} height={500} data-aos = "fade-right"   data-aos-offset="200"/>
+        <p className="text-[1.2rem]" data-aos = "fade-left"   data-aos-offset="200">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, praesentium aspernatur illum necessitatibus sed recusandae nostrum accusantium nemo ullam odit?</p>
      </div>
     </div>   
    <div className="h-[25rem] w-full flex justify-center items-center">
      <div className="flex items-center justify-center w-[60rem] pl-16">
-        <p className="text-[1.5rem]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, praesentium aspernatur illum necessitatibus sed recusandae nostrum accusantium nemo ullam odit?</p>
-        <img src="image_two.jpg" alt="image"  width={500} height={500}/>
+        <p className="text-[1.2rem]" data-aos="fade-up-right" data-aos-offset="200">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, praesentium aspernatur illum necessitatibus sed recusandae nostrum accusantium nemo ullam odit?</p>
+        <img src="image_two.jpg" alt="image"  width={400} height={400} data-aos="fade-up-left" data-aos-offset="200"/>
      </div>
     </div>  
    </>
